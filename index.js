1313a5c1b3bd22a72b8a435a2755b2bb9a36f6f5
Nodirr <162604287+Nodirr@users.noreply.github.com>
2024-05-11 11:28:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let btn = document.querySelector('.btn')
+let ekran = document.querySelector('.ekran')
+let deleteBtn = document.querySelector('.delete')
+let svet = document.querySelector('.let')
+let main = document.querySelector('main')
+
+
+btn.addEventListener('click', function(){
+    ekran.value = parseInt(ekran.value) + 1;
+    console.log(ekran.value)
+if (ekran.value >=15 && ekran.value < 30){
+    main.style.backgroundColor = 'blue'
+ 
+} else if (ekran.value >= 30 && ekran.value < 50) {
+    main.style.backgroundColor = 'red'
+  }
+  else if (ekran.value >= 50 && ekran.value < 60) {
+    main.style.backgroundColor = 'purple'
+  }
+})
+
+deleteBtn.addEventListener('click', function(){
+    let restart = 0
+    ekran.value = restart
+    console.log(restart);
+})
+
+svet.addEventListener('click', function(){
+    ekran.classList.toggle('inp2')
+})
+
